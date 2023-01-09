7f2dcf024fd32c6a19d864343d1867ef60df4367
Your Name <marinabananas@gmail.com>
2023-01-09 22:00:12 +0300

    first

---
+++ query_musicians.sql
@@ -0,0 +1,13 @@
+-- Database: Musicians_full
+
+-- DROP DATABASE IF EXISTS "Musicians_full";
+
+CREATE DATABASE "Musicians_full"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Russian_Russia.1251'
+    LC_CTYPE = 'Russian_Russia.1251'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
